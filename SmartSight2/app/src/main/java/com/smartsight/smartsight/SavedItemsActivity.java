61a7fc345c0e1b5a9f48cb1ef0bee3993a74191d
Phillipe's Coffee House <wissamchidiac17@gmail.com>
2026-03-30 21:56:45 +0300

    Add files via upload

---
+++ SmartSight2/app/src/main/java/com/smartsight/smartsight/SavedItemsActivity.java
@@ -0,0 +1,7 @@
+package com.example.smartsight;
+
+import android.os.Bundle; import androidx.appcompat.app.AppCompatActivity;
+public class SavedItemsActivity extends AppCompatActivity {
+    @Override protected void onCreate(Bundle b) { super.onCreate(b);
+        setContentView(R.layout.activity_saved_items); }
+}
---
+++ SmartSight2/app/src/main/java/com/smartsight/smartsight/SmartScanActivity.java
@@ -0,0 +1,7 @@
+package com.example.smartsight;
+
+import android.os.Bundle; import androidx.appcompat.app.AppCompatActivity;
+public class SmartScanActivity extends AppCompatActivity {
+    @Override protected void onCreate(Bundle b) { super.onCreate(b);
+        setContentView(R.layout.activity_smart_scan); }
+}
