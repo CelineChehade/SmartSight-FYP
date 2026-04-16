52e19f7873b031d259b61df020ef4c9110de9cff
Phillipe's Coffee House <wissamchidiac17@gmail.com>
2026-04-17 00:08:36 +0300

    Add files via upload

---
+++ SmartSight2/app/src/main/java/com/smartsight/smartsight/NoteViewModel.java
@@ -0,0 +1,29 @@
+package com.example.smartsight;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class NoteViewModel extends AndroidViewModel {
+
+    private final AppRepository repository;
+    private final LiveData<List<SavedNote>> allNotes;
+
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+        repository = new AppRepository(application);
+        allNotes   = repository.getAllNotes();
+    }
+
+    public LiveData<List<SavedNote>> getAllNotes() {
+        return allNotes;
+    }
+
+    public void delete(SavedNote note) {
+        repository.deleteNote(note);
+    }
+}
